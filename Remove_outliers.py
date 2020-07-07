691f9be5dbcff4ec2a6474a2f8f1637c7721d846
sohaibdelhi <67917510+sohaibdelhi@users.noreply.github.com>
2020-07-07 15:07:53 +0530

    Update Remove_outliers.py

--- Remove_outliers.py
+++ Remove_outliers.py
@@ -5,8 +5,8 @@ def remove_outliers(df):
         if (((df[col].dtype)=='float64') | ((df[col].dtype)=='int64')):
             ''' The percentiles values contain the range values for 0.05 and 0.95'''
             percentiles_values = df[col].quantile([0.05,0.95]).values
-            df[col][df[col] <= percentiles[0]] = percentiles_values[0]
-            df[col][df[col] >= percentiles[1]] = percentiles_values[1]
+            df[col][df[col] <= percentiles_values[0]] = percentiles_values[0]
+            df[col][df[col] >= percentiles_values[1]] = percentiles_values[1]
         else:
             df[col]=df[col]
     return df
